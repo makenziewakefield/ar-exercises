65c4b38386823170104a20e32896eafd4d14fda0
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-08-27 17:04:01 -0600

    Set up one to many relationship between stores and employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "Bob", last_name: "Johnson", hourly_rate: 45)
+@store2.employees.create(first_name: "Alice", last_name: "Williams", hourly_rate: 65)
