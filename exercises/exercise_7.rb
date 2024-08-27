46b0738958f1af6522f598855ca112bfb3d7fb7d
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-08-27 17:04:47 -0600

    Add validations for store and employee models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,21 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages
+
